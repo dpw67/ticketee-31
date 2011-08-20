3e96a5ea3d53771e8d70538a4c4e863c58a47b30
Doug Warren <git@dpw67.oib.com>
2011-08-20 01:35:04 -0500

    Added rate limiting to the V1 API

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::BaseController < ActionController::Base
   respond_to :json, :xml
 
   before_filter :authenticate_user
+  before_filter :check_rate_limit
 
   private
     def authenticate_user
@@ -12,10 +13,6 @@ class Api::V1::BaseController < ActionController::Base
       end
     end
 
-    def current_user
-      @current_user
-    end
-
     def authorize_admin!
       if !@current_user.admin?
         if !@current_user.admin?
@@ -23,6 +20,19 @@ class Api::V1::BaseController < ActionController::Base
           warden.custom_failure!
           render params[:format].to_sym => error , :status => 401
         end
+      end
+    end
+
+    def current_user
+      @current_user
+    end
+
+    def check_rate_limit
+      if @current_user.request_count > 100
+        error = { :error => "Rate limit exceeded." }
+        respond_with(error, :status => 403)
+      else
+        @current_user.increment!(:request_count)
       end
     end
 
